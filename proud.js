ee912265070fd1b814132578ccf1bb775ae75da9
shawneeboyote <54691499+shawneeboyote@users.noreply.github.com>
2023-07-12 19:55:51 -0500

    Update proud.js

--- proud.js
+++ proud.js
@@ -1,3 +1,1 @@
-} else if (skipPartition){
-return path.slice(-1) === "/" ? path : `${path}/`; // if user did't provide trailing slash, add one.
-}
+return path.slice(-1) === "/" ? path + file : `${path}/${file}`;
