92a14c11612d0fe4999cc0394c9c8f4e86371188
shawneeboyote <54691499+shawneeboyote@users.noreply.github.com>
2024-01-29 15:03:53 -0600

    Update proud.js

--- proud.js
+++ proud.js
@@ -1,1 +1,2 @@
+// Don't laugh. I copied this from a repo I worked on years ago because it was the first enhancement I independently discovered and implemented in production. Short and simple, but very meaningful to me.
 return path.slice(-1) === "/" ? path + file : `${path}/${file}`;
